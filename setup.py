5c52feb5f4751e8e9ded7c842b1adbf423b08227
fuwen <fuwenquant@gmail.com>
2026-04-22 16:32:38 +0800

    update

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import os
 from setuptools import setup, find_packages
 
 # ===================== 核心：版本号会被 publish.sh 自动更新 =====================
-PIP包名 = "fuwen"
-版本号 = "1.0.2"
+PIP包名 = "fuwin"
+版本号 = "1.0.1"
 一句话描述 = "fuwenquant 的一个PYPI测试包（包含add_one和helloworld函数）"
 
 
